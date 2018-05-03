6ed528c73c8ec316d699eaeef1db22aff82b1d0a
Reid Laitinen <reidlaitinen@gmail.com>
2018-05-03 10:33:11 -0600

    home component layout

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { Component, Fragment } from 'react';
+import { Header, Grid, Input, Menu } from 'semantic-ui-react';
 
 class Home extends Component {
+
+  state = {activeItem: null}
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
+    const { activeItem } = this.state
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <Fragment>
+        <Header as='h1' textAlign='center'>KeyBook</Header>
+        <Grid columns='equal'>
+          <Grid.Column>
+            <Menu vertical>
+              <Menu.Item name='search' active={activeItem === 'search'} onClick={this.handleItemClick}>
+                Search
+              </Menu.Item>
+              <Menu.Item name='allDevices' active={activeItem === 'allDevices'} onClick={this.handleItemClick}>
+                All Devices
+              </Menu.Item>
+              <Menu.Item name='userMgmt' active={activeItem === 'userMgmt'} onClick={this.handleItemClick}>
+                User Management
+              </Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Input fluid icon='search' placeholder='Search...' autofocus='true' />
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+        </Grid>
+      </Fragment>
+
     );
   }
 }
