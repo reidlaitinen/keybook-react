ccdada3dd67cead278145852444c4c7b875c345a
Reid Laitinen <reidlaitinen@gmail.com>
2018-05-06 14:30:49 -0600

    stubbed out DeviceList

---
+++ client/src/components/DeviceList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react';
+
+class DeviceList extends React.Component {
+
+  render() {
+    return <Header as='h2'>Device List Component</Header>
+  }
+
+}
+
+export default DeviceList;
